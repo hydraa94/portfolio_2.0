86d548a1e874158a4d4493297ddba36f87a04098
hydraa94 <nailuldev2948@gmail.com>
2025-08-16 21:35:22 +0700

    Remaking API in Hono

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,61 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
+import { config } from "dotenv";
+import { Octokit } from "octokit";
+import { cors } from "hono/cors";
+import axios from "axios";
 
-const app = new Hono()
+config();
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/hono'
-]
+const githubKey = process.env.GITHUB_TOKEN;
+const itchKey = process.env.ITCH_API_KEY;
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
+const octokit = new Octokit({ auth: githubKey });
+const app = new Hono();
 
-export default app
+async function getRepos() {
+  const { data } = await octokit.request("GET /user/repos", {
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+
+  const refinedData = data.map((repo) => ({
+    id: repo.id,
+    title: repo.name,
+    description: repo.description,
+    url: repo.html_url,
+    platform: "GitHub",
+  }));
+
+  return refinedData;
+}
+
+async function getProjects() {
+  const { data } = await axios.get(`https://itch.io/api/1/${itchKey}/my-games`);
+
+  const refinedData = data.games.map((game: any) => ({
+    id: game.id,
+    title: game.title,
+    description: game.short_text,
+    image: game.cover_url,
+    url: game.url,
+    platform: "Itch.io",
+  }));
+  return refinedData;
+}
+
+app.use(cors());
+app.use(prettyJSON());
+app.get("/", (c) => {
+  return c.text("/");
+});
+
+app.get("/projects", async (c) => {
+  const reposData = await getRepos();
+  const projectsData = await getProjects();
+
+  return c.json([...reposData, ...projectsData]);
+});
+
+export default app;
